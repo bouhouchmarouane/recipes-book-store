e14a2bcb89ceaeabceac5e417e53e4cc5039339f
marouanebhch <bouhouch.marouane@gmail.com>
2020-12-29 00:17:29 +0100

    Make header functional

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Tabs} from '../tabs.enum';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() activeTab = new EventEmitter<string>();
+  eTabs = Tabs;
+  selectedTab = this.eTabs.Recipes;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  goto(tab: string): void {
+    this.activeTab.emit(tab);
+    this.selectedTab = tab;
+  }
 }
