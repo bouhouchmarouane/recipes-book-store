108002b43774091c4a804104e26ef1efe023dbbf
marouanebhch <bouhouch.marouane@gmail.com>
2020-12-30 21:18:05 +0100

    move recipes list to recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../../shared/recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,17 +8,14 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('Test1', 'This is simply a test1', 'https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg'),
-    new Recipe('Test2', 'This is simply a test2', 'https://cookieandkate.com/images/2020/03/vegan-chana-masala-recipe-2-550x824.jpg'),
-    new Recipe('Test3', 'This is simply a test3', 'https://www.cookwithmanali.com/wp-content/uploads/2014/08/Poha-Recipe.jpg')
-  ];
+  recipes: Recipe[] = [];
 
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.recipes;
   }
 
   onSelectedRecipe(recipe: Recipe): void {
