3b43fa8c32e03acff4f2f1dc67f2e8134cf51d0f
marouanebhch <bouhouch.marouane@gmail.com>
2021-01-05 21:42:58 +0100

    Editing and deleting ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,20 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
   ingredientForm: FormGroup;
+  editIngSub: Subscription;
+  editMode = false;
+  idIngredient: number;
+
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
@@ -17,19 +22,45 @@ export class ShoppingEditComponent implements OnInit {
       name: new FormControl(null, Validators.required),
       amount: new FormControl(null, [Validators.required, Validators.pattern('^[0-9]*$'), Validators.min(1)])
     });
+
+    this.editIngSub = this.shoppingListService.editingIngredientSub.subscribe(idIng => {
+      if (idIng !== null) {
+        this.editIngredient(idIng);
+      }
+    });
   }
 
-  addIngredient(name: string, amount: string): void {
-    this.shoppingListService.addIngredient(new Ingredient(name, parseInt(amount)));
-  }
-
-  onSubmit(): void {
-    console.log(this.ingredientForm);
+  addSaveIngredient(): void {
     const name = this.ingredientForm.get('name')?.value;
     const amount = this.ingredientForm.get('amount')?.value;
-    this.shoppingListService.addIngredient(new Ingredient(name, amount as number));
-    //this.ingredientForm.reset();
+    this.shoppingListService.addEditIngredient(new Ingredient(this.editMode ? this.idIngredient : null, name, amount as number));
+    this.resetForm();
   }
 
-  numeric
+  editIngredient(idIng: number): void {
+    this.idIngredient = idIng;
+    const ingredient = this.shoppingListService.findIngredientById(idIng);
+    this.ingredientForm.setValue({
+      name: ingredient.name,
+      amount: ingredient.amount
+    });
+    this.editMode = true;
+  }
+
+  ngOnDestroy(): void {
+    this.editIngSub.unsubscribe();
+  }
+
+  resetForm(): void {
+    this.ingredientForm.reset();
+    this.editMode = false;
+    this.shoppingListService.editingIngredientSub.next(null);
+  }
+
+  deleteIngredient(): void {
+    if (confirm('Are you sure to delete this ingredient ? ')) {
+      this.shoppingListService.deleteIngredient(this.idIngredient);
+    }
+    this.resetForm();
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  // tslint:disable-next-line:variable-name
   private _ingredients: Ingredient[] = [
-    new Ingredient('Apple', 5),
-    new Ingredient('Tomato', 4),
-    new Ingredient('Cheese', 1),
+    new Ingredient(0, 'Apple', 5),
+    new Ingredient(1, 'Tomato', 4),
+    new Ingredient(2, 'Cheese', 1),
   ];
+
+  editingIngredientSub = new Subject<number | null>();
 
   constructor() { }
 
@@ -17,8 +21,32 @@ export class ShoppingListService {
     return this._ingredients;
   }
 
-  addIngredient(ingredient: Ingredient): void {
-    this._ingredients.push(ingredient);
+  findIngredientById(idIng: number): Ingredient {
+    return this.ingredients.find(ing => {
+      return ing.id === idIng;
+    }) as Ingredient;
   }
 
+  addEditIngredient(ingredient: Ingredient): void {
+    if (ingredient.id !== null) {
+      const index = this.ingredients.indexOf(this.findIngredientById(ingredient.id));
+      this.ingredients[index] = ingredient;
+    } else {
+      ingredient.id = this.nextId;
+      this._ingredients.push(ingredient);
+    }
+  }
+
+  get nextId(): number {
+    // @ts-ignore
+    let maxId = Math.max.apply(Math, this.ingredients.map((ing: Ingredient) => {
+      return ing.id;
+    }));
+    return ++maxId;
+  }
+
+  deleteIngredient(idIng: number): void {
+    const index = this.ingredients.indexOf(this.findIngredientById(idIng));
+    this.ingredients.splice(index, 1);
+  }
 }
