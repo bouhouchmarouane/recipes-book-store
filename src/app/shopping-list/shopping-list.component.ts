67a64be8b89779ab8ed0f44bd7202f5e929abf49
marouanebhch <bouhouch.marouane@gmail.com>
2021-01-30 23:35:07 +0100

    Select shopping list using NgRx

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
+import {Store} from '@ngrx/store';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,14 +10,16 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[] = [];
+  ingredients: Observable<{ingredients: Ingredient[]}>;
   editingIngId: number;
   editIngSub: Subscription;
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService, private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) { }
 
   ngOnInit(): void {
-    this.ingredients = this.shoppingListService.ingredients;
+    // this.ingredients = this.shoppingListService.ingredients;
+    this.ingredients = this.store.select('shoppingList');
+
     // @ts-ignore
     this.editIngSub = this.shoppingListService.editingIngredientSub.subscribe((idIng: number) => this.editingIngId = idIng);
   }
