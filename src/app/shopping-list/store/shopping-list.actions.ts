a9a601a924ca412c61dd329ebdbb8052ed0de1bc
marouanebhch <bouhouch.marouane@gmail.com>
2021-01-31 21:33:37 +0100

    Adding delete and update actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import {Ingredient} from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENTS = 'DELETE_INGREDIENTS';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
@@ -16,3 +18,14 @@ export class AddIngredients implements Action {
   constructor(public payload: Ingredient[]) {}
 }
 
+export class UpdateIngredients implements Action {
+  readonly type = UPDATE_INGREDIENT;
+
+  constructor(public payload: {ingredient: Ingredient, index: number}) {}
+}
+
+export class DeleteIngredients implements Action {
+  readonly type = DELETE_INGREDIENTS;
+
+  constructor(public payload: number) {}
+}
