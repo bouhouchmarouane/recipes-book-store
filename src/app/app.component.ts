d6a2dd29cca571d7ac1b9e636f51cd22c24b13ea
marouanebhch <bouhouch.marouane@gmail.com>
2021-03-05 20:43:43 +0100

    Adding Angular universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {AuthService} from './auth/auth.service';
 import {Store} from '@ngrx/store';
 import {AppState} from './store/app.reducer';
 import {AutoLogin} from './auth/store/auth.actions';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,12 @@ import {AutoLogin} from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private store: Store<AppState>) {
+  constructor(private store: Store<AppState>, @Inject(PLATFORM_ID) private platformId: any) {
   }
 
   ngOnInit(): void {
-    this.store.dispatch(new AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AutoLogin());
+    }
   }
 }
